10dbdcf0d69b924bbeb20df7a6d7e80fa49d09e8
Wout De Puysseleir <woutdp@gmail.com>
2022-09-16 10:41:05 -0700

    Add Buy Me a Coffee link

--- src/lib/ui/menu/AboutMenu.svelte
+++ src/lib/ui/menu/AboutMenu.svelte
@@ -11,9 +11,10 @@
     {/if}
 
     <p>Thank you for playing!</p>
-    <p>
+    <p class="mb-4">
         It would mean a lot to me if you gave me your feedback and answered <a class="font-bold underline" target="_blank" href="https://forms.gle/gXjQB1KHA4vZNND19">4 quick questions</a>
     </p>
-    <p class="mt-4">Contribute on <a class="underline" target="_blank" href="https://github.com/woutdp/geoquest">Github</a></p>
-    <p class="mt-4">For any enquiries: <a class="underline" target="_blank" href="mailto:geoquest@wout.space">geoquest@wout.space</a></p>
+    <p class="">Contribute on <a class="underline" target="_blank" href="https://github.com/woutdp/geoquest">Github</a></p>
+    <p class="">For any enquiries: <a class="underline" target="_blank" href="mailto:geoquest@wout.space">geoquest@wout.space</a></p>
+    <a class="underline font-bold" target="_blank" href="https://www.buymeacoffee.com/woutdp">Buy Me a Coffee</a>
 </div>
