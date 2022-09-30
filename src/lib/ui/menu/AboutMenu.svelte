de06abe4d6900c52874fb4b24f040172f901833e
Wout De Puysseleir <woutdp@gmail.com>
2022-09-29 18:35:27 -0700

    Improve about section

--- src/lib/ui/menu/AboutMenu.svelte
+++ src/lib/ui/menu/AboutMenu.svelte
@@ -1,20 +1,37 @@
 <script lang="ts">
-    import {scale} from 'svelte/transition'
+    import IconEmail from '$lib/icons/IconEmail.svelte'
+    import IconGithub from '$lib/icons/IconGithub.svelte'
+    import IconLightbulb from '$lib/icons/IconLightbulb.svelte'
+    import IconWorld from '$lib/icons/IconWorld.svelte'
+
+    const dev = import.meta.env.DEV
+
     $: inIframe = window.location !== window.parent.location
 </script>
 
-<div class="p-2 rounded-md bg-foreground">
-    {#if inIframe}
-        <p in:scale class=" bg-background text-foreground p-2 text-center rounded-md text-md mb-2">
-            Play GeoQuest on <a class="font-black underline" target="_blank" href="https://geoquest.wout.space/">geoquest.wout.space</a> for better performance.
-        </p>
-    {/if}
-
-    <p>Thank you for playing!</p>
-    <p class="mb-4">
-        It would mean a lot to me if you gave me your feedback and answered <a class="font-bold underline" target="_blank" href="https://forms.gle/gXjQB1KHA4vZNND19">4 quick questions</a>
+<div class="bg-foreground-light rounded-xl p-5 flex flex-col gap-y-2 mb-2">
+    <p class="font-bold">Thank you for playing!</p>
+    <p class="mb-0">
+        It would mean a lot to me if you answered <a class="font-bold underline" target="_blank" href="https://forms.gle/gXjQB1KHA4vZNND19">4 quick questions</a>
     </p>
-    <p class="">Contribute on <a class="underline" target="_blank" href="https://github.com/woutdp/geoquest">Github</a></p>
-    <p class="">For any enquiries: <a class="underline" target="_blank" href="mailto:geoquest@wout.space">geoquest@wout.space</a></p>
-    <a class="underline font-bold" target="_blank" href="https://www.buymeacoffee.com/woutdp">Buy Me a Coffee</a>
+    <hr class="mb-4 mt-3 text-gray" />
+    <a class="font-medium rounded-full bg-foreground p-2 min-w-full flex items-center hover:bg-foreground hover:scale-[1.02] transition-all" target="_blank" href="https://geoquest.sleekplan.app/">
+        <span class="ml-4 mr-2 text-gray"><IconLightbulb /></span>Suggestions
+    </a>
+    <a class="font-medium rounded-full bg-foreground p-2 min-w-full flex items-center hover:bg-foreground hover:scale-[1.02] transition-all" target="_blank" href="https://github.com/woutdp/geoquest">
+        <span class="ml-4 mr-2 text-gray"><IconGithub /></span>Contribute
+    </a>
+    <a class="font-medium rounded-full bg-foreground p-2 min-w-full flex items-center hover:bg-foreground hover:scale-[1.02] transition-all" target="_blank" href="mailto:geoquest@wout.space">
+        <span class="ml-4 mr-2 text-gray"><IconEmail /></span>geoquest@wout.space
+    </a>
+    <div class="min-w-full bg-brand-buy-me-a-coffee rounded-full hover:scale-[1.02] transition-all">
+        <a href="https://www.buymeacoffee.com/woutdp" target="_blank" class="flex">
+            <img class="h-[47px] w-[170px] ml-4" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" />
+        </a>
+    </div>
+    {#if inIframe || dev}
+        <a class="bg-background text-foreground p-3 rounded-full hover:scale-[1.02] transition-all flex items-center" href="https://geoquest.wout.space/">
+            <span class="ml-3 mr-2 text-foreground"><IconWorld /></span><span class="font-black underline">geoquest.wout.space</span>
+        </a>
+    {/if}
 </div>
