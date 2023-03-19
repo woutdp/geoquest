6ced5722f2d34c2e4e5120e436b763969ecfe7dc
Wout De Puysseleir <woutdp@gmail.com>
2023-03-19 10:39:39 -0700

    WIP: This is a work in progress commit

---
+++ src/routes/get-local-storage/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {onMount} from 'svelte'
+
+    let permittedDomain = 'https://geoquest.gg'
+
+    onMount(async () => {
+        console.log(localStorage)
+        window.addEventListener('message', function (event) {
+            if (event.origin === permittedDomain) {
+                if (event.data) {
+                    localStorage.setItem('localstorage', event.data)
+                } else {
+                    localStorage.removeItem('localstorage')
+                }
+            }
+        })
+    })
+</script>
