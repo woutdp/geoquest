c9da49f60cb91e85ecc5da2884aa7278a4542a16
Jonas <git.jonas@vpo.me>
2024-12-25 13:32:35 +0000

    Configure translation loading

---
+++ src/routes/[[locale]]/+layout.ts
@@ -0,0 +1,12 @@
+import {loadTranslations} from '$lib/translations'
+import type {LayoutLoad} from './$types'
+
+export const load: LayoutLoad = async ({url, params}) => {
+    const {pathname} = url
+
+    const initLocale = params.locale || 'en' // get from cookie, user session, ...
+
+    await loadTranslations(initLocale, pathname) // keep this just before the `return`
+
+    return {}
+}
