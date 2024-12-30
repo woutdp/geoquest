6fa04c4f2a6362a619eec4eb350830a4cb599cd1
Jonas <git.jonas@vpo.me>
2024-12-30 13:51:51 +0000

    Set fallbacklocale to variable

--- src/routes/[[locale]]/+layout.ts
+++ src/routes/[[locale]]/+layout.ts
@@ -1,13 +1,12 @@
-import {loadTranslations} from '$lib/translations'
+import {fallbackLocale, loadTranslations} from '$lib/translations'
 
 import type {LayoutLoad} from './$types'
 
 export const load: LayoutLoad = async ({url, params}) => {
     const {pathname} = url
 
-    const initLocale = params.locale || 'en' // get from cookie, user session, ...
-
-    await loadTranslations(initLocale, pathname) // keep this just before the `return`
+    const initLocale = params.locale || fallbackLocale
+    await loadTranslations(initLocale, pathname)
 
     return {}
 }
