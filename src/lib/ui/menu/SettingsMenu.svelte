f28c974720d8877d6c280453cc202103f7852ed5
Jonas <git.jonas@vpo.me>
2024-12-31 12:30:28 +0000

    Add settings to locale config

--- src/lib/ui/menu/SettingsMenu.svelte
+++ src/lib/ui/menu/SettingsMenu.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-    import emojiFlags from 'emoji-flags'
-
     import {showFlagOnly} from '$lib/store'
-    import {t} from '$lib/translations'
+    import {availableLocales, t} from '$lib/translations'
 </script>
 
 <div class="flex flex-col">
     <div class="flex flex-col mb-4 justify-between gap-2">
         <div class="flex flex-wrap text-lg bg-foreground p-3 rounded items-center">
             <input id="displayFlagOnly" class="form-checkbox h-6 w-6 rounded text-gray focus:ring-0 focus:ring-offset-0 transition cursor-pointer" type="checkbox" bind:checked={$showFlagOnly} />
-            <label for="displayFlagOnly" class="ml-2 cursor-pointer">{$t('geoquest.ui.flags-only')}</label>
+            <label for="displayFlagOnly" class="ml-2 cursor-pointer">{$t('ui.flagsOnly')}</label>
         </div>
         <div class="flex flex-wrap gap-2 text-lg bg-foreground p-3 rounded items-center">
             <!-- Each of the language names is only directly written in their own language -->
-            <a href="/"> <button>{emojiFlags.GB.emoji} English</button></a>
-            <a href="/de"><button>{emojiFlags.DE.emoji} Deutsch</button></a>
-            <a href="/es"><button>{emojiFlags.ES.emoji} Español</button></a>
+            {#each Object.entries(availableLocales) as [locale, [name, flag]] (locale)}
+                <a href="/{locale}"> <button>{flag} {name}</button></a>
+            {/each}
         </div>
     </div>
 </div>
