00274175b1484d1d2b5aa1ce6d36a117533e7dd6
squeak <squeak@eauchat.org>
2025-01-14 17:21:07 +0100

    Refactoring to make it possible to play other quests. Added the following additional quests: finding USA states, Chinese provinces, French departments.

--- src/lib/ui/MouseTooltip.svelte
+++ src/lib/ui/MouseTooltip.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import emojiFlags from 'emoji-flags'
 
-    import {clientX, mousePos} from '$lib/store'
+    import {clientX, mousePos, chosenMap} from '$lib/store'
     import {t} from '$lib/translations'
 
     export let focusedCountry
@@ -18,6 +18,6 @@
 {#if show}
     <span bind:clientHeight={h} class="fixed z-50 inline-block px-2 py-1 font-bold rounded-md pointer-events-none bg-background-dark whitespace-nowrap" style:left="{x}px" style:top="{y - h}px">
         {#if flag}{flag}{/if}
-        {$t(`countries.${focusedCountry?.properties?.name}`)}
+        {$t(`${chosenMap.id}-elements.${focusedCountry?.properties?.name}`)}
     </span>
 {/if}
