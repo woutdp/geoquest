d372aa658b3e9f98942941ea686d663d20c0dba0
Wout De Puysseleir <woutdp@gmail.com>
2025-02-10 13:25:16 -0800

    Add POST api webhook buymeacoffee

---
+++ src/routes/api/webhooks/coffee/+server.ts
@@ -0,0 +1,44 @@
+import {json} from '@sveltejs/kit'
+import {dev} from '$app/environment'
+
+export async function POST({request, platform}) {
+    const secret = platform.env.COFFEE_WEBHOOK_SECRET || 'SECRET'
+    const signature = request.headers.get('x-signature-sha256')
+
+    if (!signature) return json({error: 'missing signature'}, {status: 400})
+
+    const rawBody = await request.text()
+
+    if ((await generateSignature(secret, rawBody)) !== signature && !dev) {
+        return json({error: 'Invalid signature'}, {status: 401})
+    }
+
+    let result = await platform.env.DB.prepare('SELECT * FROM users').run()
+
+    const data = JSON.parse(rawBody)
+
+    if (data.type === 'donation.created') {
+        const email = data.data.supporter_email
+        await platform.env.DB.prepare('INSERT INTO users (email) VALUES (?)').bind(email).run()
+        return json({success: true}, {status: 200})
+    }
+
+    if (data.type === 'donation.refunded') {
+        const email = data.data.supporter_email
+        await platform.env.DB.prepare('DELETE FROM users WHERE email=(?)').bind(email).run()
+        return json({success: true}, {status: 200})
+    }
+
+    return json({success: false}, {status: 204})
+}
+
+async function generateSignature(secret, rawBody) {
+    const encoder = new TextEncoder()
+    const keyData = encoder.encode(secret)
+    const key = await crypto.subtle.importKey('raw', keyData, {name: 'HMAC', hash: 'SHA-256'}, false, ['sign'])
+    const dataBuffer = encoder.encode(rawBody)
+    const signatureBuffer = await crypto.subtle.sign('HMAC', key, dataBuffer)
+    return Array.from(new Uint8Array(signatureBuffer))
+        .map(b => b.toString(16).padStart(2, '0'))
+        .join('')
+}
